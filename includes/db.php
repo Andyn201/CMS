ee7429b7926e85c983c226edc88aedad8dc6cae9
Seryph <seryph@Seryphs-MacBook-Pro.local>
2020-03-24 14:52:17 -0500

    db

---
+++ includes/db.php
@@ -0,0 +1,22 @@
+<?php
+
+# DB variables
+
+$db['db_host'] = 'localhost';
+$db['db_user'] = 'root';
+$db['db_pass'] = 'root';
+$db['db_name'] = 'cms';
+
+# Make all DB variables into constants
+
+foreach ($db as $key => $value) {
+	define(strtoupper($key), $value);
+}
+
+# Connect To DB
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if($connection) {
+	echo "Connected";
+}
