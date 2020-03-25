bcf1e9e295896ff69743ead45eb4c2083fb8f78d
Seryph <seryph@Seryphs-MacBook-Pro.local>
2020-03-24 19:24:04 -0500

    k

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,33 +1,10 @@
 <div class="col-md-4">
 
-<?php
-
-    if(isset($_POST['search'])){
-        $search = $_POST['search'];
-        
-        $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' ";
-
-        $search_query = mysqli_query($connection,$query);
-
-        if(!$search_query) {
-            die('Query Failed' . mysqli_error($connection));
-        }
-
-        $count = mysqli_num_rows($search_query);
-        if($count == 0) {
-            echo '<h1>No result</h1>';
-        }
-        else {
-            echo '<h1>Result</h1>';
-        }
-    }
-
-?>
 
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
-                    <form action="" method="POST">
+                    <form action="search.php" method="POST">
                         <div class="input-group">
                             <input name="search" type="text" class="form-control">
                             <span class="input-group-btn">
